99c68476f672832b1edac6a327153b9585f91785
Volcano-Yang <648941183@qq.com>
2020-07-02 10:05:23 +0800

    修改头文件

--- fs.h
+++ fs.h
@@ -7,59 +7,63 @@
 // 关于磁盘的定义
 
 //系统大小100m
-#define SYSTEMSIZE 100*1024*1024   
+#define MEM_D_SIZE 512*1024*1024
 //盘块大小，按kb划分
-#define BLOCKSIZE 1024 
-//系统盘块数目
-#define BLOCKCOUNT SYSTEMSIZE/BLOCKSIZE 
+#define DISKSIZE 1024
+#define DISK_NUM 1024
+#define FATSIZE DISK_NUM*sizeof(struct fatitem)
+#define ROOT_DISK_NO FATSIZE/(DISKSIZE+1)
+#define ROOT_DISK_SIZE sizeof(struct direct)
+#define DIR_MAXSIZE 1024
+#define MSD 10
+#define MOFN 5
+#define MAX_WRITE 1024*128
 
-void start();
-//磁盘分配
-int getBlock(int blockSize);
-//获得盘块的物理地址
-char* getBlockAddr(int blockNum);
-//获得物理地址盘块号
-int getAddr(char* addr);
-//释放盘块
-int releaseBlock(int blockNum, int blockSize);
-//系统起始地址
-char* systemStartAddr;  
-// #endif
-
-// 以下是
-// 关于文件系统操作的定义
-#define dirMaxSize 15
-
-// 文件控制块
-struct FCB {
-    int protect; // 权限标志, 0为不可读不可写，1为只读，2为可读可写
-    int readptr;    //读指针，字节为单位
-    int blockNum;   //文件数据起始盘块号
-    int fileSize;   //文件大小，盘块为单位
-    int dataSize;   //已写入的内容大小，字节为单位
+struct fatitem {
+	int item;
+	char em_disk;
 };
-// 文件或目录结构：
-struct dirFile{
-    char fileName[32];  //文件名
-    int startBlock; //FCB起始盘块
-    char* time; // 创建时间
-    int type; // 0为文件 1为目录
+struct direct {
+	struct FCB {
+		char name[9];
+		char property;
+		int size;
+		int firstdisk;
+		int next;
+		int sign;
+	}directitem[MSD + 2];
 };
-
-//目录表结构：
-struct dir {
-    int total; // 目录项数目
-    struct dirFile items[dirMaxSize];// 目录中的文件列表
+struct opentable {
+	struct openttableitem {
+		char name[9];
+		int firstdisk;
+		int size;
+	}opeitem[MOFN];
+	int cur_size;
 };
- 
-
-struct dir *rootdir; // 根目录
-struct dir *currentDir; // 当前目录
-
-void ls();
-
-void start();
-void do_main();
-void helpInfo();
+struct fatitem *fat;
+struct direct *root;
+struct direct *cur_dir;
+struct opentable u_opentable;
+int fd = -1;
+char *bufferdir;
+char *fdisk;
+void initfile();
+void format();
+void enter();
+void halts();
+int create(char *name);
+int open(char *name);
+int close(char *name);
+int write(int fd, char *buf, int len);
+int read(int fd, char *buf);
+int del(char *name);
+int mkdir(char *name);
+int rmdir(char *name);
+void dir();
+int cd(char *name);
+void print();
+void show();
 int main();
+
 #endif
